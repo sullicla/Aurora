f42b53436e1ec9ecf37328566f93f8298e127b30
sulli <sullicla@uw.edu>
2023-05-18 22:19:49 -0700

    about, climate, and visualization updates

--- visualizations/journey_timeline/timeline.js
+++ visualizations/journey_timeline/timeline.js
@@ -1,48 +1,25 @@
 $(function(){
-    $().timeline({
-        orientation: 	'vertical',
-        issuesSpeed: 	300,
-        datesSpeed: 	100,
-        arrowKeys: 		'true',
-        startAt:		3
-    })
-});
+  $('#issues').timelinr({
+      orientation: 'vertical',
+      issuesSpeed: 300,
+      datesSpeed: 100,
+      arrowKeys: true,
+      startAt: 3
+  });
 
-// Get references to the next and previous buttons
-var nextButton = document.getElementById('next');
-var prevButton = document.getElementById('prev');
+  // Get a reference to the Timelinr instance
+  var timelinr = $('#issues').data('plugin_timelinr');
 
-// Get references to the list of issues and the currently selected issue
-var issuesList = document.getElementById('issues');
-var selectedIssue = document.querySelector('#issues li.selected');
+  // Get references to the next and previous buttons
+  var nextButton = document.getElementById('next');
+  var prevButton = document.getElementById('prev');
 
-// Add event listeners for the next and previous buttons
-nextButton.addEventListener('click', function() {
-  // Get the next issue element
-  var nextIssue = selectedIssue.nextElementSibling;
-  if (nextIssue) {
-    // If there is a next issue, remove the selected class from the current issue
-    selectedIssue.classList.remove('selected');
-    // Add the selected class to the next issue
-    nextIssue.classList.add('selected');
-    // Set the scroll position of the issues list to show the next issue
-    issuesList.scrollLeft += nextIssue.offsetLeft - issuesList.offsetLeft;
-    // Update the selectedIssue variable
-    selectedIssue = nextIssue;
-  }
-});
+  // Add event listeners for the next and previous buttons
+  nextButton.addEventListener('click', function() {
+      timelinr.next();
+  });
 
-prevButton.addEventListener('click', function() {
-  // Get the previous issue element
-  var prevIssue = selectedIssue.previousElementSibling;
-  if (prevIssue) {
-    // If there is a previous issue, remove the selected class from the current issue
-    selectedIssue.classList.remove('selected');
-    // Add the selected class to the previous issue
-    prevIssue.classList.add('selected');
-    // Set the scroll position of the issues list to show the previous issue
-    issuesList.scrollLeft -= selectedIssue.offsetLeft - prevIssue.offsetLeft;
-    // Update the selectedIssue variable
-    selectedIssue = prevIssue;
-  }
+  prevButton.addEventListener('click', function() {
+      timelinr.prev();
+  });
 });
